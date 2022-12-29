787c40e84d64f7b6c07ba8df52ea6a5da4233d2d
Piyush9100 <piyush9100@gmail.com>
2022-12-29 20:31:07 +0530

    updated other info

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+    var myLatLng = {lat: 23.0225, lng: 72.5714};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
@@ -10,6 +10,6 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'South Jakarta, INA' // Title Location
+        title: 'Piyush Patel, Ahmedabad' // Title Location
     });
 }
